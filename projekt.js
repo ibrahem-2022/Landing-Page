9447872681f91f1ccaad9c168f8e72a21a5caf2b
ibrahem-2022 <107585316+ibrahem-2022@users.noreply.github.com>
2022-12-20 23:30:16 +0200

    Update projekt.js

--- projekt.js
+++ projekt.js
@@ -1,11 +1,12 @@
 let navBar = document.querySelector("#navbar__list");
+let sections = document.querySelectorAll("section");
 let nav = function (){
-    let sections = document.querySelectorAll("section");
+    
     let bar="";
         sections.forEach(section=>{
             let o = {
                 sectionId : section.id,
-                sectionDataNav : section.dataset.nav
+                sectionDataNav : section.dataset.nav,
             }
             const li= `<li><a href="#${o.sectionId}" data-nav="${o.sectionId}" class="menu__link">${o.sectionDataNav}</a></li>`;
             bar += li
@@ -16,18 +17,35 @@ nav()
 
 
 window.onscroll = function() {
-	let x =  document.querySelectorAll("section").forEach(function(activeall) {
-    let Links = navBar.querySelector(`[data-nav=${activeall.id}]`);
-    if(activeall.getBoundingClientRect().top >= -400 && activeall.getBoundingClientRect().top <= 150){
+	document.querySelectorAll("section").forEach(function (activeall) {
+    
+    if(
+        activeall.getBoundingClientRect().top >= -400&& 
+        activeall.getBoundingClientRect().top <= 150
+        ){
         activeall.classList.add("your-active-class");
-        Links.classList.add("active-link");
-    }
-    else{
+        } else{
         activeall.classList.remove("your-active-class");
-        Links.classList.remove("active-link");
     }
 	});
 }
 
+
+
+
+
+
+
+
+function scrollEvent(no) {
+    let section = document.getElementById('section' + no);
+    let position = section.offsetTop;
+    event.preventDefault();
+    window.scrollTo({
+        left: 0,
+        top: position,
+        behavior: 'smooth'
+    })
+}
 
 
